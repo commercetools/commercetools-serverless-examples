1d4b014bac1e2f23ac31e76e1a44d230720b3df2
tdeekens <nerd@tdeekens.name>
2018-02-27 13:35:30 +0100

    chore: format via prettier

--- packages/example-inventory/002-connecting-ct/index.js
+++ packages/example-inventory/002-connecting-ct/index.js
@@ -1,14 +1,18 @@
 // require CT sdk libraries
-const createClient = require('@commercetools/sdk-client').createClient
-const createHttpMiddleware = require('@commercetools/sdk-middleware-http').createHttpMiddleware
-const createQueueMiddleware = require('@commercetools/sdk-middleware-queue').createQueueMiddleware
-const createAuthMiddlewareForClientCredentialsFlow = require('@commercetools/sdk-middleware-auth').createAuthMiddlewareForClientCredentialsFlow
-const createRequestBuilder = require('@commercetools/api-request-builder').createRequestBuilder
-const createLoggerMiddleware = require('@commercetools/sdk-middleware-logger').createLoggerMiddleware
+const createClient = require("@commercetools/sdk-client").createClient;
+const createHttpMiddleware = require("@commercetools/sdk-middleware-http")
+  .createHttpMiddleware;
+const createQueueMiddleware = require("@commercetools/sdk-middleware-queue")
+  .createQueueMiddleware;
+const createAuthMiddlewareForClientCredentialsFlow = require("@commercetools/sdk-middleware-auth")
+  .createAuthMiddlewareForClientCredentialsFlow;
+const createRequestBuilder = require("@commercetools/api-request-builder")
+  .createRequestBuilder;
+const createLoggerMiddleware = require("@commercetools/sdk-middleware-logger")
+  .createLoggerMiddleware;
 
 // import config from config.json
-const config = require('./config.json');
-
+const config = require("./config.json");
 
 /**
  * Provides a client to communicate with commercetools.
@@ -22,27 +26,28 @@ const createCTClient = () => {
         projectKey: config.CT_PROJECT_KEY,
         credentials: {
           clientId: config.CT_CLIENT_ID,
-          clientSecret: config.CT_CLIENT_SECRET,
+          clientSecret: config.CT_CLIENT_SECRET
         },
-        scopes: config.CT_SCOPES.indexOf(' ') > 0 ? config.CT_SCOPES.split(' ') : [config.CT_SCOPES],
+        scopes:
+          config.CT_SCOPES.indexOf(" ") > 0
+            ? config.CT_SCOPES.split(" ")
+            : [config.CT_SCOPES]
       }),
       createHttpMiddleware({
-        host: config.CT_API_HOST,
+        host: config.CT_API_HOST
       }),
-      createLoggerMiddleware(),
-    ],
-   })
-  return client
-}
+      createLoggerMiddleware()
+    ]
+  });
+  return client;
+};
 
 /**
  * Creates a request builder
  */
 const createCTRequestBuilder = () => {
-  return createRequestBuilder({ projectKey: config.CT_PROJECT_KEY })
-}
-
-
+  return createRequestBuilder({ projectKey: config.CT_PROJECT_KEY });
+};
 
 /**
  * Responds to any HTTP request that can provide a "sku" field in the body.
@@ -54,26 +59,33 @@ exports.helloWorld = function helloWorld(req, res) {
   // Example input: {"sku": "cool-product-123"}
   if (req.body.sku === undefined) {
     // This is an error case, as "sku" is required.
-    res.status(400).send('No sku defined!');
+    res.status(400).send("No sku defined!");
   } else {
     // Everything is okay.
     // Let's attempt to look up the inventory for that sku
-    const requestBuilder = createCTRequestBuilder()
-    const inventoryUri = requestBuilder.inventory.where(`sku="${req.body.sku}"`).build()
+    const requestBuilder = createCTRequestBuilder();
+    const inventoryUri = requestBuilder.inventory
+      .where(`sku="${req.body.sku}"`)
+      .build();
     const inventoryRequest = {
       method: "GET",
       uri: inventoryUri
-    }
-    const client =createCTClient()
-    client.execute(inventoryRequest).then(result => {
-      const results = result.body.results
-      if (!results.length) {
-        return res.status(404).json({error:'Inventory for SKU not found!'})
-      }
-      res.status(200).json(results)
-    }).catch(e => {
-      console.error(e)
-      res.status(400).json(e)
-    })
+    };
+    const client = createCTClient();
+    client
+      .execute(inventoryRequest)
+      .then(result => {
+        const results = result.body.results;
+        if (!results.length) {
+          return res
+            .status(404)
+            .json({ error: "Inventory for SKU not found!" });
+        }
+        res.status(200).json(results);
+      })
+      .catch(e => {
+        console.error(e);
+        res.status(400).json(e);
+      });
   }
 };
--- packages/extension-base/index.js
+++ packages/extension-base/index.js
@@ -1,4 +1,4 @@
-var adapter = require('./ctp-extension-azure-adapter.js');
+var adapter = require("./ctp-extension-azure-adapter.js");
 module.exports = adapter.ctpExtensionAdapter(addInsurance);
 
 // var adapter = require('./ctp-extension-gcf-adapter.js');
@@ -11,39 +11,39 @@ function addInsurance(input, ctpResponse, log) {
   var cart = input.resource.obj;
   // If the cart contains any line item that is worth more than $500,
   // mandatory insurance needs to be added.
-  var cartRequiresInsurance = cart.lineItems.some(lineItem =>
-    lineItem.totalPrice.centAmount > 50000
+  var cartRequiresInsurance = cart.lineItems.some(
+    lineItem => lineItem.totalPrice.centAmount > 50000
   );
-  var insuranceItem = cart.customLineItems.find( (customLineItem) => {
+  var insuranceItem = cart.customLineItems.find(customLineItem => {
     return customLineItem.slug == "mandatory-insurance";
   });
-  var cartHasInsurance = insuranceItem != undefined
+  var cartHasInsurance = insuranceItem != undefined;
 
   if (cartRequiresInsurance && !cartHasInsurance) {
     log("adding insurance");
-    ctpResponse.accept([{
-      action: "addCustomLineItem",
-      name: { en: "Mandatory Insurance for Items above $500" },
-      money: {
-        currencyCode: cart.totalPrice.currencyCode,
-        centAmount: 1000
-      },
-      slug: "mandatory-insurance",
-      taxCategory: {
-        typeId: "tax-category",
-        id: taxCategoryId
+    ctpResponse.accept([
+      {
+        action: "addCustomLineItem",
+        name: { en: "Mandatory Insurance for Items above $500" },
+        money: {
+          currencyCode: cart.totalPrice.currencyCode,
+          centAmount: 1000
+        },
+        slug: "mandatory-insurance",
+        taxCategory: {
+          typeId: "tax-category",
+          id: taxCategoryId
+        }
       }
-    }]);
-  }
-  else if (!cartRequiresInsurance && cartHasInsurance) {
+    ]);
+  } else if (!cartRequiresInsurance && cartHasInsurance) {
     log("removing insurance");
     ctpResponse.accept({
       action: "removeCustomLineItem",
       customLineItemId: insuranceItem.id
     });
-  }
-  else {
+  } else {
     log("nothing to do");
     ctpResponse.accept();
   }
-};
+}
