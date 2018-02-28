0785a25f88e2c7ccb7c1c29ad7123ee447ced17e
tdeekens <nerd@tdeekens.name>
2018-02-28 10:20:41 +0100

    refactor: rename export

--- packages/extension-base/ctp-extension-azure-adapter.js
+++ packages/extension-base/ctp-extension-azure-adapter.js
@@ -26,7 +26,7 @@ const ctpResponse = ctx => ({
 });
 
 module.exports = {
-  create: fn => (ctx, req) => {
+  createExtensionAdapter: fn => (ctx, req) => {
     fn(req.body, ctpResponse(ctx), ctx.log);
   },
 };
